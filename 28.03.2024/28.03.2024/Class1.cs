ccda9f3bb987d093fb8e21dd85950d4ffe949a38
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-03-28 00:06:20 +0400

    initial commit

---
+++ 28.03.2024/28.03.2024/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._03._2024
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+
+
+            Student student1 = new Student("Rauf", "Dostuyev", "AB106", 95, true);
+            student1.GetInfo();
+            student1.CheckGraduation();
+            student1.GetDiplomaDegree();
+
+            Console.WriteLine();
+
+            Student student2 = new Student("Sabir", "Guliyev", "AB106", 67, false);
+            student2.GetInfo();
+            student2.CheckGraduation();
+            student2.GetDiplomaDegree();
+
+            Student student3 = new Student("Sahib", "Mammadov", "AB106", 35, true);
+            student3.GetInfo();
+            student3.CheckGraduation();
+            student3.GetDiplomaDegree();
+
+            Student student4 = new Student("Novruzali", "Aliyev", "AB106", 87, false);
+            student4.GetInfo();
+            student4.CheckGraduation();
+            student4.GetDiplomaDegree();
+
+            Student student5 = new Student("Abbasov", "Abbas", "AB106", 99, true);
+            student5.GetInfo();
+            student5.CheckGraduation();
+            student5.GetDiplomaDegree();
+
+            Student student6 = new Student("Shams", "Babayeva", "AB106", 71, true);
+            student6.GetInfo();
+            student6.CheckGraduation();
+            student6.GetDiplomaDegree();
+        }
+    }
+}
---
+++ 28.03.2024/28.03.2024/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _28._03._2024
+{
+    internal class Program
+    {
+
+    }
+    public class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+        public Student(string name, string surname, string group, int point, bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isGraduated;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Group: {Group}");
+            Console.WriteLine($"Point: {Point}");
+            Console.WriteLine($"Is Graduated: {(IsGraduated ? "Yes" : "No")}");
+        }
+
+        public void CheckGraduation()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine($"{Name} {Surname} Mezun olub.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} {Surname} Mezun olmayib.");
+            }
+        }
+
+        public void GetDiplomaDegree()
+        {
+            string degree = Point switch
+            {
+                int p when p < 65 => "Standart",
+                int p when p >= 65 && p < 80 => "Heveslendirici",
+                int p when p >= 80 && p < 90 => "Zerbeci",
+                int p when p >= 90 => "Elaci",
+                _ => throw new ArgumentException("Invalid point value"),
+            };
+
+            Console.WriteLine($"{Name} {Surname} {degree} Ile Diplom Qiymetlendirilib.");
+        }
+
+    }
+
+}
